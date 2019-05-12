e09c3716da836b2a53d053e3c8735222b97732d4
Joel Brown <joelbrown90@gmail.com>
2019-05-12 01:19:05 -0400

    readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # reactjs-portfolio
+
+This is my Portfolio site which is a full stack project. The technologies used in this project are ReactJs/NextJs, Node, MongoDB.
+
+AuthO is use to login in to make a dynamic unique site to post resume and blogs.
+
+CRUD functionality is use to for client to interact with the blog page.
---
+++ components/SuperComponent.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import BaseLayout from '../components/layouts/BaseLayout'
+class SuperComponent extends React.Component {
+    render() {xs
+      constructor() {
+        this.someVariable = 'just some variable'
+      }
+
+      return (
+        <BaseLayout>
+          <h1>I am blog Page from class component</h1>
+        </BaseLayout>
+    )
+  }
+}
+
+export default SuperComponent;
